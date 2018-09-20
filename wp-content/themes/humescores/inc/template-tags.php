b7e0463cea9fde58dfa3473f006d13fd35ff5f36
Rustam4457 <punk60826@gmail.com>
2018-09-20 21:36:58 +0300

    styling single post

--- wp-content/themes/humescores/inc/template-tags.php
+++ wp-content/themes/humescores/inc/template-tags.php
@@ -6,7 +6,6 @@
  *
  * @package Humescores
  */
-
 if ( ! function_exists( 'humescores_posted_on' ) ) :
 	/**
 	 * Prints HTML with meta information for the current post-date/time.
@@ -16,25 +15,20 @@ if ( ! function_exists( 'humescores_posted_on' ) ) :
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 		}
-
 		$time_string = sprintf( $time_string,
 			esc_attr( get_the_date( DATE_W3C ) ),
 			esc_html( get_the_date() ),
 			esc_attr( get_the_modified_date( DATE_W3C ) ),
 			esc_html( get_the_modified_date() )
 		);
-
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
 			esc_html_x( 'Posted on %s', 'post date', 'humescores' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
-
 		echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
-
 	}
 endif;
-
 if ( ! function_exists( 'humescores_posted_by' ) ) :
 	/**
 	 * Prints HTML with meta information for the current author.
@@ -45,12 +39,9 @@ if ( ! function_exists( 'humescores_posted_by' ) ) :
 			esc_html_x( 'by %s', 'post author', 'humescores' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
-
 		echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
-
 	}
 endif;
-
 if ( ! function_exists( 'humescores_entry_footer' ) ) :
 	/**
 	 * Prints HTML with meta information for the categories, tags and comments.
@@ -58,7 +49,6 @@ if ( ! function_exists( 'humescores_entry_footer' ) ) :
 	function humescores_entry_footer() {
 		// Hide tag text for pages.
 		if ( 'post' === get_post_type() ) {
-
 			/* translators: used between list items, there is a space after the comma */
 			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'humescores' ) );
 			if ( $tags_list ) {
@@ -66,7 +56,6 @@ if ( ! function_exists( 'humescores_entry_footer' ) ) :
 				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'humescores' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 			}
 		}
-
 		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 			echo '<span class="comments-link">';
 			comments_popup_link(
@@ -85,7 +74,6 @@ if ( ! function_exists( 'humescores_entry_footer' ) ) :
 			);
 			echo '</span>';
 		}
-
 		edit_post_link(
 			sprintf(
 				wp_kses(
@@ -104,7 +92,6 @@ if ( ! function_exists( 'humescores_entry_footer' ) ) :
 		);
 	}
 endif;
-
 if ( ! function_exists( 'humescores_post_thumbnail' ) ) :
 	/**
 	 * Displays an optional post thumbnail.
@@ -116,7 +103,6 @@ if ( ! function_exists( 'humescores_post_thumbnail' ) ) :
 		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
 			return;
 		}
-
 		if ( is_singular() ) :
 			?>
 
