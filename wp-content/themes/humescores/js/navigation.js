1b38e4058a84b260c1018da5871439ad2ae1d027
Rustam4457 <punk60826@gmail.com>
2018-09-20 21:03:53 +0300

    display category list in single.php

--- wp-content/themes/humescores/js/navigation.js
+++ wp-content/themes/humescores/js/navigation.js
@@ -8,28 +8,28 @@
 (function( $ ) {
 	var masthead, menuToggle, siteNavigation;
 
-	function initMainNavigation( container ) {
-		var dropdownToggle = $( '<button />', { 'class': 'dropdown-toggle', 'aria-expanded': false })
-			.append( $( '<span />', { 'class': 'dropdown-symbol', text: '+' }) )
-			.append( $( '<span />', { 'class': 'screen-reader-text', text: humescoresScreenReaderText.expand }) );
+	// function initMainNavigation( container ) {
+	// 	var dropdownToggle = $( '<button />', { 'class': 'dropdown-toggle', 'aria-expanded': false })
+	// 		.append( $( '<span />', { 'class': 'dropdown-symbol', text: '+' }) )
+	// 		.append( $( '<span />', { 'class': 'screen-reader-text', text: humescoresScreenReaderText.expand }) );
 
-		container.find( '.menu-item-has-children > a, .page_item_has_children > a' ).after( dropdownToggle );
+	// 	container.find( '.menu-item-has-children > a, .page_item_has_children > a' ).after( dropdownToggle );
 
-		container.find( '.dropdown-toggle' ).click( function( e ) {
-			var _this = $( this ),
-				screenReaderSpan = _this.find( '.screen-reader-text' );
-				dropdownSymbol = _this.find( '.dropdown-symbol' );
-				dropdownSymbol.text( dropdownSymbol.text() === '-' ? '+' : '-');
+	// 	container.find( '.dropdown-toggle' ).click( function( e ) {
+	// 		var _this = $( this ),
+	// 			screenReaderSpan = _this.find( '.screen-reader-text' );
+	// 			dropdownSymbol = _this.find( '.dropdown-symbol' );
+	// 			dropdownSymbol.text( dropdownSymbol.text() === '-' ? '+' : '-');
 
-			e.preventDefault();
-			_this.toggleClass( 'toggled-on' );
-			_this.next( '.children, .sub-menu' ).toggleClass( 'toggled-on' );
+	// 		e.preventDefault();
+	// 		_this.toggleClass( 'toggled-on' );
+	// 		_this.next( '.children, .sub-menu' ).toggleClass( 'toggled-on' );
 
-			_this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
+	// 		_this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
 
-			screenReaderSpan.text( screenReaderSpan.text() === humescoresScreenReaderText.expand ? humescoresScreenReaderText.collapse : humescoresScreenReaderText.expand );
-		});
-	}
+	// 		screenReaderSpan.text( screenReaderSpan.text() === humescoresScreenReaderText.expand ? humescoresScreenReaderText.collapse : humescoresScreenReaderText.expand );
+	// 	});
+	// }
 
 
 	initMainNavigation( $( '.main-navigation' ) );
