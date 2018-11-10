b7ce32bb0c1db38ba242dd2931b47dbebdb4e566
Rustam4457 <punk60826@gmail.com>
2018-11-11 01:37:43 +0200

    modify metadata

--- wp-content/themes/humescores/footer.php
+++ wp-content/themes/humescores/footer.php
@@ -20,7 +20,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'humescores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'humescores' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme:.', 'humescores' ), 'humescores'); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'humescores' ), 'humescores', '<a href="https://github.com/heneros" rel="designer">Heneros</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
